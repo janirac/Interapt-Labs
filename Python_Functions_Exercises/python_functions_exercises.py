0f4c702a860025b251ebbaa43313fbc4017805af
Janira Crispin <janiracrispin@gmail.com>
2023-09-12 15:59:28 -0400

    complete maximum of three numbers function

---
+++ Python_Functions_Exercises/python_functions_exercises.py
@@ -0,0 +1,8 @@
+# Write a Python function to find the maximum of three numbers 
+
+def maximum(num_1, num_2, num_3):
+    max_num = max([num_1, num_2, num_3])
+    
+    return max_num
+    
+print(maximum(1, 2, 3))
