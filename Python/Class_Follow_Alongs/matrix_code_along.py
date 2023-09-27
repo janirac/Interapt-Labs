3cd3a199187515b5b5c8962867bab8ce93fc4a6e
Janira Crispin <janiracrispin@gmail.com>
2023-09-27 10:23:08 -0400

    update class follow along matrix

--- Python/Class_Follow_Alongs/matrix_code_along.py
+++ Python/Class_Follow_Alongs/matrix_code_along.py
@@ -1,84 +1,65 @@
-''' Poor man's implementation of a 2-D matrix class
-    Pass a list of numbers and a TUPLE of #rows/#cols to CTOR
-    Matrix object has 3 properties:
-    
-    _rows = number of rows
-    _ columns = number of columns
-    _data = elements of the matrix
-    
-    my_mat = Matrix( [1,2,3,4,5,6], (2,3) ) yields a 2 X 3 matrix
-    printable representation:
-    
-    | 1 2 |
-    | 3 4 |
-    | 5 6 |
- 
-    Let's forgo creating properties for now (if we have time, maybe)     
-'''
+# Poor man's implementation of a 2-D matrix class
+import operator
 
 class Matrix:
-    def __init__(self, data, order):
-        if len(order) != 2 or type(order[0] != int or type(order[1]) != int):
-            raise ValueError(f'Elements in {order} not numeric or wrong # of elements passed in')
-        
-        if order[0] * order[1] != len(data):
-            raise ValueError(f'Dimension mismatch between {order} and {data}')
-        
-        if any([type(elem) not in (int, float) for elem in data]):
-            raise ValueError(f'Not all entires in {order} are numeric')
-        
     '''
         data is a list. order is a tuple of (rows, columns)
-        Maybe explore some arg checking?
+        Maybe explore additional arg checking?
     '''
-    # Code the CTOR here
-    
+    def __init__( self, data, order ):
         # Should we check if 'order' is a tuple of two INTEGERS???
-        
-        # Check if order arg matches data dimensions. If not, raise exception
-        # Remember that order is a TUPLE
- 
-        # Check if ALL entries are numeric. If not, raise exception
+        if not all ([isinstance(elem, int) for elem in order]) or len(order) != 2:
+            raise ValueError ("Dimensions passed to CTOR are not integral or two elements not passed for dimensions")
 
+        # Check if order arg matches data dimensions
+        self._rows = order[0]
+        self._columns = order[1]
+        if len_data := len(data) != self._rows * self._columns:
+            raise ValueError (f'data passed has {len_data} elements; does not match'
+                              f'dimensions ({self._rows} X {self._columns}')  
+        # Check if ALL entries are numeric
+        if any([type(elem) not in (int, float) for elem in data]):
+            raise TypeError('Not all values in data passed to CTOR are numeric')
         
-        # OK that's enough - let's load the matrix by copying data arg into instance variable
+        # OK that's enough - let's load the matrix
+        self._data = data.copy()
                 
-    # Let's print it out. Use the dunder overload
- 
-        # Initialize an empty string and concatenate data and row delimiters
-        #
-        # Let's think about what we want this string to look like when done
-        # Note each line is enclosed in |   |
-        # We need a way of indexing the elements in the _data instance by
-        # calculating something using the _rows and _columns instance variables
-        #
-        # The printed result will have _rows printed, so we could use some sort of loop?
-        #
-        # Inside that loop, we want to concatenate elements from _data based on
-        # a calculation using rows/columns
-        # Maybe some examples will help?
-        #
-        # for a 2 X 3 matrix, we want these indexes from _data:
-        # 0  1      Each loop iteration produces a row with these index elements from _data
-        # 2  3
-        # 4  5
-        #
-        # For a 4 X 3
-        #
-        # 0 1 2 3
-        # 4 5 6 7
-        # 8 9 10 11
-        #
-        # Surround each row with a "|" 
+    # Let's print it out
+    def __str__(self):
+        mat_str = ""
+        for row in range(self._rows):
+            start_idx = row * self._columns
+            end_idx = (row + 1) * self._columns
+            mat_str += "| " + " ".join([str(self._data[elem]) for elem in range(start_idx, end_idx)]) + " |\n"
+            
+        return mat_str
     
-    # Add two matrices - let's look up the dunder used to overload '+'
-     
+    def _operate_on_matrices(self, rho_mat, operation):
         # Check if rows and columns of both match
-        
+        if self._rows != rho_mat._rows or self._columns != rho_mat._columns:
+            raise ValueError ('Matrix dimensions incompatible')
         # All good...let's add elements, return result
-        # Any thoughts on how to generate a structure that allows us to easily add
-        # the numbers in the Matrix objects?
-
+        return Matrix([operation(elem1, elem2) for (elem1, elem2) in zip(self._data, rho_mat._data)], 
+                             (self._rows, self._columns))
+        
+    
+    # Add two matrices
+    def __add__(self, rho_mat):       
+        return self._operate_on_matrices(rho_mat, operator.add)
+    
+    # Subtract two matrices
+    def __sub__(self, rho_mat):       
+        return self._operate_on_matrices(rho_mat, operator.sub)  
+    
+    # Multiply (dot product, actually) two matrices
+    def __mul__(self, rho_mat):       
+        return self._operate_on_matrices(rho_mat, operator.mul)      
+    
+    # Divide (element divide) two matrices
+    def __truediv__(self, rho_mat):       
+        return self._operate_on_matrices(rho_mat, operator.truediv)     
+        
+        
         
 if __name__ == "__main__":
     mat = Matrix([1,2,3,4,5,6], (3, 2))
@@ -86,20 +67,17 @@ if __name__ == "__main__":
     
     print("Matrix 'mat':")
     print(mat)
-    '''       
-    | 1 2 |
-    | 3 4 |
-    | 5 6 |
-    '''
     print("Matrix 'mat2':")
     print(mat2)    
-    '''       
-    | 11 12 |
-    | 13 14 |
-    | 15 16 |
-    '''    
     # Add them, print result 
-    sum_mats = mat + mat2
-    
+    sum_mats = mat + mat2  
     print("Sum of the above matrices:")
-    print(sum_mats) 
+    print(sum_mats)  
+    diff_mats = sum_mats - mat
+    print("Difference of the above matrices:")
+    print(diff_mats)  
+    dot_prod_mats = sum_mats * mat
+    print("Dot product of the above matrices:")
+    print(dot_prod_mats)     
+         
+
